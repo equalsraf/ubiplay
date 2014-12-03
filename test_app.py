900f9c2dcb620a762901ec543d567e2ffd031db7
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-12-03 19:18:09 +0000

    Test: REST services return 503 error responses
    - When one of the REST services responds and the MPD server is not
      available it uses status code 503 and a json object with an 'error'
      attribute

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
-import unittest
+import unittest, json
 from app import app
 
-class TestUbiPlayApp(unittest.TestCase):
+class TestNoMDP(unittest.TestCase):
+    """
+    Tests for when MPD is not present
+    """
     
     def setUp(self):
         self.app = app.test_client()
@@ -10,6 +13,28 @@ class TestUbiPlayApp(unittest.TestCase):
         """Frontpage returns 200"""
         resp = self.app.get('/')
         self.assertEquals(resp.status_code, 200)
+
+    def test_ws(self):
+        """REST services return error object w/ code 503"""
+        for rule in app.url_map.iter_rules():
+
+            # ignore /stattic, and non-GET routes
+            if rule.endpoint in ('static',):
+                continue
+            if rule.arguments:
+                continue
+            if not 'GET' in rule.methods:
+                continue
+            # / is the frontpage, skip it
+            if rule.rule == '/':
+                continue
+            resp = self.app.get(rule.rule)
+            self.assertEquals(resp.status_code, 503)
+
+            # See python issue #10976
+            data = json.loads(resp.data.decode(resp.charset or 'utf8'))
+            self.assertIn('error', data)
+
 
 if __name__ == '__main__':
     unittest.main()
